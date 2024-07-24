3abd086cc21d3b0b88030875401c72e68c19de2e
ad <hi@arnaudiaz.com>
2024-07-24 12:57:56 +0200

    coverage repo

--- src/test/java/com/github/vrnvu/java_todo_list/todos/TodoRepositoryInMemoryTest.java
+++ src/test/java/com/github/vrnvu/java_todo_list/todos/TodoRepositoryInMemoryTest.java
@@ -2,6 +2,9 @@ package com.github.vrnvu.java_todo_list.todos;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TodoRepositoryInMemoryTest {
@@ -17,5 +20,22 @@ class TodoRepositoryInMemoryTest {
         assertEquals(todo.id(), "0");
         assertEquals(todo.title(), title);
         assertFalse(todo.completed());
+    }
+
+    @Test
+    void getNotFound() {
+        var todoRepository = new TodoRepositoryInMemory();
+        Optional<Todo> todo = todoRepository.getTodo("not-found");
+
+        assertFalse(todo.isPresent());
+    }
+
+    @Test
+    void getAll() {
+        var todoRepository = new TodoRepositoryInMemory();
+        todoRepository.createTodo("title-id");
+        List<Todo> todos = todoRepository.getTodos();
+        assertNotNull(todos);
+        assertEquals(todos.size(), 1);
     }
 }
